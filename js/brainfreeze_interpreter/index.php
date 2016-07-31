6731a0a842e79ea8b8c0d26386ee1a52556fec81
Spencer Bartz <spencerbartz@gmail.com>
2016-07-30 18:04:06 -0700

    Improve BrainFreeze instructions and explanation

--- js/brainfreeze_interpreter/index.php
+++ js/brainfreeze_interpreter/index.php
@@ -28,10 +28,11 @@
             <h3><?php echo _("What is BrainFreeze?"); ?></h3>
             <p>
                 <div id="description">
-                BrainFreeze is a programming language that consists of merely 8 characters: { [ ] > < . , + - } but can be used to create an infinite number programs. I thought it would be an interesting challenge
-                to try and make an interpreter that could both parse and run the BrainFreeze code. BrainFreeze is an exact clone of <a href="https://en.wikipedia.org/wiki/Brainfuck">this programming language</a>.
+                BrainFreeze is a programming language that consists of merely 8 characters: { [ ] > < . , + - } but can be used to create an infinite number programs.<br/><br/>
+                The input area on the left receives normal text and converts into brain freeze. The input area on the right converts regular text back to BrainFreeze.<br/><br/>
+                BrainFreeze is an exact clone of <a href="https://en.wikipedia.org/wiki/Brainfuck">this programming language</a>.
                 However, this is a professional portfolio so I renamed it. Technically BrainFreeze is a subset of its parent language since it does not support input.
-                It will be added in the next iteration to make BrainFreeze a truly Turing-Complete language. Refer to the wikipedia page for instructions on usage.
+                It will be added in the next iteration to make BrainFreeze a truly Turing-Complete language. Refer to the <a href="https://en.wikipedia.org/wiki/Brainfuck">wikipedia page</a> for instructions on usage.
                 </div>
             </p>
         </div>    
@@ -41,7 +42,7 @@
             <h3><?php echo _("Text to BrainFreeze Converter"); ?></h3>
             <p>
             <textarea id="conv_input" class="rounded" placeholder="Type a message in plain text then press the Convert button to see the resulting BrainFreeze code below."></textarea>
-            <input id="submit_conv_input" type="submit" class="rounded" value="Convert" onclick="BFI.asciiToBF('conv_input')" />
+            <input id="submit_conv_input" type="submit" class="rounded" value="Convert to BrainFreeze Code" onclick="BFI.asciiToBF('conv_input')" />
             </p>
             <p>
             <textarea id="conv_output" class="rounded"></textarea>
@@ -58,7 +59,7 @@
             </p>
             <p>
             <textarea id="intrp_output" class="rounded"></textarea>
-            <input id="clear_intrp_output" type="submit" class="rounded" value="Clear" onclick="clearTextArea('intrp_output')"/>
+            <input id="clear_intrp_output" type="submit" class="rounded" value="Clear" onclick="clearTextArea('intrp_output');"/>
             </p>
         </div>    
     </div>
